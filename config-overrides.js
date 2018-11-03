ffa9c53da2ad5a9d59e910daeca3f41497aeb23d
David Salgado <dsalgado@spredfast.com>
2018-11-03 09:11:01 -0500

    Changed primary color to pink.

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,18 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireCssModules = require('react-app-rewire-css-modules');
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
     config = injectBabelPlugin(
-        ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
+        ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], // change importing css to less
         config,
     );
+
+    config = rewireLess.withLoaderOptions({
+        modifyVars: { "@primary-color": "#E13BDE" },
+        javascriptEnabled: true,
+    })(config, env);
+
     config = rewireCssModules(config, env);
     return config;
 };
